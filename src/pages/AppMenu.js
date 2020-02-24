cbd41744e8c6b321ce4aebe0bd3b75f0aaba69fc
Harshit Sinha <>
2020-02-24 19:21:37 +0530

    app menu style added

--- src/pages/AppMenu.js
+++ src/pages/AppMenu.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { Button } from "grommet";
-import { Login, Help } from "grommet-icons";
+import { Button ,Box} from "grommet";
+import { Next } from "grommet-icons";
 
 function AppMenu() {
   return (
     <>
-      <Button plain label="Sell Tickets" onClick={() => {}} />
+    <Box pad="medium">
+      <Button primary className="sellTicket" color="status-ok" label="Sell Tickets" onClick={() => {}} />
       <Button
         reverse
-        active
-        icon={<Login />}
+        className="appMenuButton"
+        icon={<Next />}
         label="Login"
         onClick={() => {}}
       />
       <Button
         reverse
-        active
-        icon={<Login />}
-        label="Login"
+        className="appMenuButton"
+        icon={<Next />}
+        label="How It Works"
         onClick={() => {}}
       />
-      <Button reverse active icon={<Help />} label="Help" onClick={() => {}} />
+      <Button reverse  className="appMenuButton" icon={<Next />} label="Help" onClick={() => {}} />
+      </Box>
     </>
   );
 }
