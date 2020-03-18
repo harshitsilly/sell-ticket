2cd98a115df8a81fdce6116448e87e439d0718aa
Arvind kumar <arvind@Arvinds-MacBook-Air.local>
2020-03-18 23:12:16 +0530

    location changes

--- src/components/locationContent.js
+++ src/components/locationContent.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Box, TextInput } from "grommet";
 import { Search, Down } from "grommet-icons";
+import Geosuggest from 'react-geosuggest';
+ 
+
 
 function LocaltionContent() {
   return (
     <>
-      <Box direction="row" pad="small" className="dropDownContent"></Box>
+      <Box direction="row" pad="small" className="dropDownContent">
+      <Geosuggest />
+      </Box>
     </>
   );
 }
