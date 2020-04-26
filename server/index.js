e34b77f6f6cde01ffd51c463fec6b1a318713d33
Harshit Sinha <>
2020-04-26 15:04:36 +0530

    test notification added

--- server/index.js
+++ server/index.js
@@ -108,10 +108,85 @@ server.express.use('/auth/google/callback', passport.authenticate('google', { fa
 		.redirect('/');
 });
 
+// push notification
+// ... See previous article for the rest of the code
+const bodyParser = require('body-parser');
+
+const webPush = require('web-push');
+
+// for this application we use allSubscription object to manage our subscriptions
+// this is NOT GOOD for production since it makes our application stateful
+const allSubscriptions = {};
+
+// We use webpush to generate our public and private keys
+const { publicKey, privateKey } = webPush.generateVAPIDKeys();
+
+// We are giving webpush the required information to encrypt our data
+
+webPush.setVapidDetails('mailto:harshit886@outlook.com', publicKey, privateKey);
+
+// bodyParse.json() allows us to easily read json bodies
+server.express.use(bodyParser.json());
+// Send our public key to the client
+server.express.get('/vapid-public-key', (req, res) => {
+	return res.send({ publicKey });
+});
+
+// Allows our client to subscribe
+server.express.post('/subscribe', (req, res) => {
+	const subscription = req.body;
+	registerTasks(subscription);
+	res.send('subscribed!');
+});
+
+const registerTasks = subscription => {
+	const endpoint = subscription.endpoint;
+
+	// the endpoints are the keys of our subscriptions object
+	// Every 3 seconds we will send a notification with the message 'hey this is a push!'
+	const intervalID = setInterval(() => {
+		sendNotification(subscription, 'Hey this is a push!');
+	}, 10000);
+	allSubscriptions[endpoint] = intervalID;
+};
+
+// Allows our client to unsubscribe
+server.express.post('/unsubscribe', (req, res) => {
+	const endpoint = req.body.endpoint;
+	// We find the client's endpoint and clear the interval associated with it
+	const intervalID = allSubscriptions[endpoint];
+	clearInterval(intervalID);
+	// We delete the key
+	delete allSubscriptions[endpoint];
+});
+
+// This function takes a subscription object and a payload as an argument
+// It will try to encrypt the payload
+// then attempt to send a notification via the subscription's endpoint
+const sendNotification = async (subscription, payload) => {
+	// This means we won't resend a notification if the client is offline
+	const options = {
+		TTL: 0
+	};
+
+	if (!subscription.keys) {
+		payload = payload || null;
+	}
+
+	// web-push's sendNotification function does all the work for us
+	try {
+		const res = await webPush.sendNotification(subscription, payload, options);
+		console.log(res, 'sent!');
+	} catch (e) {
+		console.log('error sending', e);
+	}
+};
+
 server.express.use(serveStatic('build'));
 server.express.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname, '../build/index.html'));
 });
+
 const options = {
 	port: process.env.PORT || 4001,
 	endpoint: '/graphql',
