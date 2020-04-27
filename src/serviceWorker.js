189a5f4be59c9a02b15362c610ce0a7226fe62d3
Harshit Sinha <>
2020-04-28 01:24:45 +0530

    push notification now works

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -48,31 +48,6 @@ export function register(config) {
 				registerValidSW(swUrl, config);
 			}
 		});
-		window.addEventListener('push', function(e) {
-			const message = e.data;
-			console.log(message);
-			// The notificationOptions will shape the look and behavior of our notification
-			const notificationOptions = {
-				body: `Time is the message: ${message}`,
-				// we use the images from the PWA generator we made
-				icon: '/sell_ticket/mipmap-xhdpi/sell_ticket.png',
-				vibrate: [100, 50, 100],
-				data: {
-					dateOfArrival: Date.now(),
-					primaryKey: '2'
-				},
-				actions: [
-					{
-						action: 'close',
-						title: 'Close'
-					}
-				]
-			};
-			e.waitUntil(
-				// We use the service worker's registration `showNotification` function to display the Notification
-				window.registration.showNotification('💊💊 You got notified! 💊💊', notificationOptions)
-			);
-		});
 	}
 }
 
